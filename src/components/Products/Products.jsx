d6823d3d75d23de8f09c3439432b8e857b1fd9b5
sherkhan61 <65385144+sherkhan61@users.noreply.github.com>
2021-01-21 22:43:23 +0600

    create Navbar component, logo, styles

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import { Grid } from '@material-ui/core'
 import Product from "./Product/Product";
 
-
+import useStyles from './styles'
 const products = [
-    {id: 1, name: 'Shoes', description: 'Running shoes', price: '$5'},
-    {id: 2, name: 'Macbook', description: 'Apple macbook', price: '$10'}
+    {id: 1, name: 'Shoes', description: 'Running shoes', price: '$5', image: 'https://assets.reebok.com/images/w_600,f_auto,q_auto/e766e993884e4e81a478ab1300dc2666_9366/Forever_Floatride_Energy_2_Women\'s_Running_Shoes_Grey_EF6909_01_standard.jpg'},
+    {id: 2, name: 'Macbook', description: 'Apple macbook', price: '$10', image: 'https://nout.kz/upload/iblock/ae7/1.jpg'}
 ]
 
 const Products = () => {
+    const classes = useStyles()
+
     return (
-        <main>
+        <main className={classes.content}>
+            <div className={classes.toolbar}/>
             <Grid container justify='center' spacing={4}>
                 {products.map((product) => (
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>

b70c12e3b7446889275c04acb309fc449f46cb5e
sherkhan61 <65385144+sherkhan61@users.noreply.github.com>
2021-01-27 00:00:38 +0600

    add button functions

--- src/App.js
+++ src/App.js
@@ -18,11 +18,28 @@ const App = () => {
         setCart(await commerce.cart.retrieve())
     }
     const handleAddToCart = async (productId, quantity) => {
-        const item = await commerce.cart.add(productId, quantity)
+        const { cart } = await commerce.cart.add(productId, quantity)
 
-        setCart(item.cart)
+        setCart(cart)
     }
 
+    const handleUpdateCartQty = async (productId, quantity) => {
+        const { cart } = await commerce.cart.update(productId, {quantity})
+
+        setCart(cart)
+    }
+
+    const handleRemoveFromCart = async (productId) => {
+        const { cart } = await commerce.cart.remove(productId)
+
+        setCart(cart)
+    }
+
+    const handleEmptyCart = async () => {
+        const { cart } = await commerce.cart.empty()
+
+        setCart(cart)
+    }
 
     useEffect(() => {
         fetchProducts()
@@ -40,7 +57,10 @@ const App = () => {
                       <Products products={products} onAddToCart={handleAddToCart} />
                   </Route>
                   <Route exact path='/cart'>
-                      <Cart cart={cart}/>
+                      <Cart cart={cart}
+                            handleUpdateCartQty={handleUpdateCartQty}
+                            handleRemoveFromCart={handleRemoveFromCart}
+                            handleEmptyCart={handleEmptyCart}/>
                   </Route>
               </Switch>
           </div>

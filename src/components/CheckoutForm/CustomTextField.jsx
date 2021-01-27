8ce67e9f8a1f3898c83d976cf2138a17cce455df
sherkhan61 <tulkibai.sherkhan@gmail.com>
2021-01-27 18:26:47 +0600

    edit AddressForm component

---
+++ src/components/CheckoutForm/CustomTextField.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { TextField, Grid } from "@material-ui/core";
+import { useFormContext, Controller } from "react-hook-form";
+
+
+const CustomTextField = () => {
+    const {control} = useFormContext()
+
+    
+    return (
+        <Grid item xs={12} sm={6}>
+            <Controller
+                as={TextField}
+                control={control}
+            />
+        </Grid>
+    )
+}
+
+export default CustomTextField
